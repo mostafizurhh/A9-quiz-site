06a2813130b2757803e1532d895109e67c6852d0
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-11 00:40:42 +0100

    dynamic route created for each topic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './layout/Main/Main';
 import Home from './components/Home/Home'
 import Statistics from './components/Statistics/Statistics'
 import Blog from './components/Blog/Blog'
+import TopicQuestion from './TopicQuestion/TopicQuestion';
 
 function App() {
   const router = createBrowserRouter([
@@ -12,9 +13,19 @@ function App() {
       element: <Main></Main>,
       children: [
         {
+          path: '/',
+          loader: async () => { return fetch('https://openapi.programming-hero.com/api/quiz') },
+          element: <Home></Home>
+        },
+        {
           path: '/home',
           loader: async () => { return fetch('https://openapi.programming-hero.com/api/quiz') },
           element: <Home></Home>
+        },
+        {
+          path: '/home/:id',
+          loader: async ({ prams }) => { return fetch(`https://openapi.programming-hero.com/api/quiz/${prams.id}`) },
+          element: <TopicQuestion></TopicQuestion>
         },
         {
           path: '/statistics',
--- src/components/AllTopics/AllTopics.js
+++ src/components/AllTopics/AllTopics.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Alltopics.css'
 
 const AllTopics = ({ topic }) => {
-    const { name, logo, total } = topic
+    const { id, name, logo, total } = topic
     // console.log(topic)
 
-    const btnclick = () => { console.log('clicked') }
+
     return (
         <div className='topics-container'>
             <img src={logo} alt="" />
             <div className='topic-info'>
                 <h5>{name}</h5>
                 <p><small>Total Questions: {total}</small></p>
-                <button onClick={btnclick}>Strat Quiz</button>
+                <button><Link to={`/home/${id}`}>Strat Quiz</Link></button>
             </div>
         </div>
     );
