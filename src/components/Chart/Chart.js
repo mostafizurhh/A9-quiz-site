9e38341f253a5ead12e6a8e912e20499379ac939
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-11 18:19:27 +0100

    chart completed

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import './Chart.css'
+
+
+const Chart = ({ requiredData }) => {
+    const data = requiredData
+    // console.log(requiredData)
+    return (
+        <div>
+
+            <LineChart className='chart' width={300} height={370} data={data}>
+                <Line type="monotone" dataKey="total" stroke="#0000ff" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
+        </div>
+    );
+};
+
+export default Chart;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Chart from '../Chart/Chart'
+
 
 const Statistics = () => {
+    const totalData = useLoaderData()
+    // console.log(totalData.data)
     return (
         <div>
-            <h2>Statistic</h2>
+            <h2>Statistics</h2>
+            <Chart
+                key={totalData.data.id}
+                requiredData={totalData.data}></Chart>
         </div>
     );
 };
